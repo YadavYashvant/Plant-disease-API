01596b105fe76b4ab9a146cadf3fcb46e7a0fd47
yashvant <yashvantyadav855@gmail.com>
2023-11-03 00:22:10 +0530

    Initial commit

---
+++ demo/src/main/java/com/yashvant/demo/PlantDiseasesApiApplication.java
@@ -0,0 +1,13 @@
+package com.yashvant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDiseasesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantDiseasesApiApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/yashvant/demo/PlantDiseasesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDiseasesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
